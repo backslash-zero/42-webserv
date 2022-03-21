2c9a7a20410ddbf573b998f3d76e76a8364a7308
lejubjoteur <quentin.gimenez003@gmail.com>
2022-03-21 15:05:59 +0100

    pre parser

---
+++ incs/conf_parser.hpp
@@ -0,0 +1,46 @@
+#ifndef PARSER_HPP
+# define PARSER_HPP
+
+# include <string>
+# include <vector>
+# include <iterator>
+# include <iostream>
+
+namespace cf {
+
+	struct SingleSetting {
+		public:
+
+		std::string		section;
+		std::string		key;
+		std::string		value;
+	};
+
+	class Parser {
+		
+		public:
+
+		std::vector<std::string>		getLineFile(std::string filename);
+		std::string						StripLine(std::string in);
+		std::vector<std::string>		ParseSection(std::string &src);
+		std::vector<std::string>		ParseFile(std::string filename);
+	};
+
+	namespace constants {
+		const char SPACE = ' ';
+		const char TAB = '\t';
+		const char HYPHEN = '-';
+		const char UNDERSCORE = '_';
+		const char EQUALS = '=';
+		const char SEMICOLON = ';';
+		const char SECTION_START = '{';
+		const char SECTION_END = '}';
+		const char QUOTE = '\"';
+		const char DECIMAL = '.';
+		const char COMMA = ',';
+		const char COMMENT_DELIM = '#';
+	}
+}
+
+
+#endif
---
+++ srcs/conf_parser.cpp
@@ -0,0 +1,109 @@
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include <stdexcept>
+
+#include "../incs/conf_parser.hpp"
+
+namespace cf {
+
+	std::vector<std::string>		Parser::getLineFile(std::string filename) {
+		std::ifstream				file(filename);
+		std::vector<std::string>	lines;
+		std::string					line;
+
+		if (!file)
+			throw std::runtime_error("file open failure");
+		while (std::getline(file, line))
+			lines.push_back(line);
+		return lines;
+	}
+
+	std::string						Parser::StripLine(std::string in) {
+		std::string	out = "";
+		bool		inQuotes = false;
+		bool		dsp = false;
+
+		for (const auto &c : in) {
+			if ((c == constants::SPACE || c == constants::TAB) && !dsp) {
+				continue;
+			}
+			dsp = true;
+			if (c == constants::COMMENT_DELIM && !inQuotes) {
+				break;
+			}
+			if (c == constants::QUOTE) {
+				inQuotes = !inQuotes;
+				out += constants::QUOTE;
+				continue;
+			}
+			out += c;
+		}
+		return out;
+	}
+
+	std::vector<std::string>		Parser::ParseSection(std::string &src) {
+		std::vector<std::string>	token;
+		std::string::iterator it = src.begin();
+		std::string::iterator ite = src.end();
+		size_t	i = 0;
+		size_t	j = 0;
+		bool		inQuotes = false;
+
+		while (it != ite) {
+			if (*it == constants::SECTION_START || *it == constants::SECTION_END || *it == constants::SEMICOLON) {
+				token.push_back(src.substr(i, 1));
+				it++;
+				i++;
+				j++;
+			}
+			while (it != ite && (*it == constants::SPACE || *it == constants::TAB)) {
+				it++;
+				i++;
+				j++;
+			}
+			if (*it == constants::QUOTE) {
+				inQuotes = !inQuotes;
+				it++;
+				j++;
+				while (it != ite && inQuotes) {
+					if (*it == constants::QUOTE)
+						inQuotes = !inQuotes;
+					it++;
+					j++;
+				}
+				if (j - i > 0)
+				token.push_back(src.substr(i, j - i));
+				i = j;
+			}
+			while (it != ite && *it != constants::SPACE && *it != constants::TAB && *it != constants::SECTION_START &&
+				*it != constants::SECTION_END && *it != constants::SEMICOLON) {
+				it++;
+				j++;
+			}
+			if (j - i > 0)
+				token.push_back(src.substr(i, j - i));
+			i = j;
+		}
+		return token;
+	}
+
+	std::vector<std::string>			Parser::ParseFile(std::string filename) {
+		std::vector<std::string> file;
+		std::string				lines = "";
+		std::vector<std::string> parsed;
+		
+
+		file = getLineFile(filename);
+		std::vector<std::string>::iterator it = file.begin();
+		std::vector<std::string>::iterator ite = file.end();
+		for ( ; it != ite; it++) {
+			lines += StripLine(*it);
+		}
+		parsed = ParseSection(lines);
+		return parsed;
+	}
+}
+
+
