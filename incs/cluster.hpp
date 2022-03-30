8f997c618e4a14037f018e7c2622f1a6c4bf4b30
lejubjoteur <quentin.gimenez003@gmail.com>
2022-03-30 12:24:54 +0200

    indentation

--- incs/cluster.hpp
+++ incs/cluster.hpp
@@ -27,43 +27,25 @@ struct s_server_config {
 class Cluster {
 
 	public:
-		Cluster();
 
-		void	exploitTokens(std::vector<std::string> &tokens);
-		bool	initCluster();
-		bool	launch();
-		void	printConfig(void);
+	Cluster();
+
+	void	exploitTokens(std::vector<std::string> &tokens);
+	bool	initCluster();
+	bool	launch();
+	void	printConfig(void);
 
 	private:
-	std::vector<s_server_config>	_serverConf;
-	std::map<int, Server *> _servers; // stores all servers created
-	std::map<int, Server *> _clients; // stores all clients connected to serv
 
-	int		_max_sk;
-	fd_set	_msfd; //master fd, handling incoming connection
-	std::vector<s_location>	_setupLocation(std::vector<std::string> &loc);
-	s_server_config			_setupServer(std::vector<std::string> &serv, std::vector<std::string> &loc);
-	void					_clearLocation(s_location lc);
-  
-	// Exceptions:
-	// 1 - File not found
-	// class fileNotFoundException : public std::exception
-	// {
-	// public:
-	// 	virtual const char *what() const throw()
-	// 	{
-	// 		return ("File not foud");
-	// 	}
-	// }
-	// // 2 - Problem with configuration
-	// class invalidArgumentException : public std::exception
-	// {
-	// public:
-	// 	virtual const char *what() const throw()
-	// 	{
-	// 		return ("Invalid configuration file");
-	// 	}
-	// };
+	std::vector<s_server_config>	_serverConf;
+	std::map<int, Server *>			_servers; // stores all servers created
+	std::map<int, Server *>			_clients; // stores all clients connected to serv
+
+	int								_max_sk;
+	fd_set							_msfd; //master fd, handling incoming connection
+	std::vector<s_location>			_setupLocation(std::vector<std::string> &loc);
+	s_server_config					_setupServer(std::vector<std::string> &serv, std::vector<std::string> &loc);
+	void							_clearLocation(s_location lc);
 };
 
 #endif
--- incs/server.hpp
+++ incs/server.hpp
@@ -3,23 +3,26 @@
 #include "webserv.hpp"
 
 class Server{
-	private:
-		int _port;
-		int _sockfd;
-		sockaddr_in sockaddr;
 
-		std::map<int, std::string> _requests; // stores requests.
 	public:
-		Server(int port);
-		~Server();
-		int		setup();
-		int		accept();
-		bool 	listenClient(int client_fd);
 
-		int 			getPort();
-		int				getSocket();
-		sockaddr_in 	getSockaddr();
+	Server(int port);
+	~Server();
+	int		setup();
+	int		accept();
+	bool 	listenClient(int client_fd);
 
-		std::string 	createResponse(int code, std::string param);
-		std::string		getHtmlFile(const std::string& path);
+	int 			getPort();
+	int				getSocket();
+	sockaddr_in 	getSockaddr();
+
+	std::string 	createResponse(int code, std::string param);
+	std::string		getHtmlFile(const std::string& path);
+
+	private:
+
+	int							_port;
+	int							_sockfd;
+	sockaddr_in					sockaddr;
+	std::map<int, std::string>	_requests; // stores requests.
 };
